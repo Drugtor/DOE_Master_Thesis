fd7db9eb268a41c38aa4a7542cce38cc9eaf2838
Drugtor <justin.gaurich@gmx.de>
2026-01-27 09:39:28 +0100

    Update and rename 01-frational_factorial to 01-frational_factorial.py
    Added variables for easy use

--- 01-frational_factorial.py
+++ 01-frational_factorial.py
@@ -1,15 +1,30 @@
 from pyDOE3 import fracfact, fracfact_opt, fracfact_by_res
 import numpy as np
 import pandas as pd
+# Variable names and ranges
+
+component_1 = "Protein"
+component_2 = "Succrose"
+component_3 = "Histamin"
+component_4 = "PEG400"
+component_5 = "PS20"
+
+range_component_1 = (-1, 1)
+range_component_2 = (-1, 1)
+range_component_3 = (-1, 1)
+range_component_4 = (-1, 1)
+range_component_5 = (-1, 1)
+
+export_path = 'c:/Users/GAURIJU1/OneDrive - Novartis Pharma AG/Documents/fractional_experiment.xlsx'
 
 
 # 1. DEFINE EXPERIMENT PARAMETERS
 factors = {
-    "Protein": (-1, 1),
-    "Succrose": (-1, 1), 
-    "Histamin": (-1, 1),
-    "PEG400": (-1, 1),
-    "PS20": (-1, 1)
+    component_1: range_component_1,
+    component_2: range_component_2, 
+    component_3: range_component_3,
+    component_4: range_component_4,
+    component_5: range_component_5
 }
 
 num_factors = 5
@@ -30,9 +45,9 @@ for factor, (low, high) in factors.items():
     df[factor] = df[factor].map({-1: low, 1: high})
 
 # 6. RANDOMIZE AND EXPORT
-df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 df.index += 1
 df.index.name = 'Run'
 df['Freezetime'] = ''
-#df.to_excel('c:/Users/GAURIJU1/OneDrive - Novartis Pharma AG/Documents/fractional_experiment.xlsx')
-print(f"Design created: {len(df)} runs")
+df['Thawtime'] = ''
+df.to_excel(export_path)
+print(df)
